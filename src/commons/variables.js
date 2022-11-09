3e5e2da9fedaa6bb7015de634067ca977d464f64
Estifanos Neway <estifanos.neway.d@gmail.com>
2022-11-09 23:30:35 +0300

    changed 'numberOfMaxApiRequestsPerMin' to 40

--- src/commons/variables.js
+++ src/commons/variables.js
@@ -3,5 +3,5 @@ module.exports = {
     defaultPort: 5000,
     verificationTokenExpiresIn: 15 * 60 * 1000,
     accessTokenExpiresIn: 10 * 60 * 1000,
-    numberOfMaxApiRequestsPerMin: 20
+    numberOfMaxApiRequestsPerMin: 40
 };
