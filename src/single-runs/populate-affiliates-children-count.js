8c880ddcdbe2e90dc50f7549ec1d6b483065ff18
Estifanos Neway <estifanos.neway.d@gmail.com>
2022-11-26 16:13:34 +0300

    fixed: added childrenCount to Affiliate

---
+++ src/single-runs/populate-affiliates-children-count.js
@@ -0,0 +1,29 @@
+// get all affiliate
+// for each affiliate
+//      get their children (count)
+//      update their childrenCount
+
+const mongoose = require("mongoose");
+const { affiliatesDb } = require("../database");
+const { affiliatesRepo } = require("../repositories");
+const { env } = require("../env");
+
+async function populate() {
+    // @ts-ignore
+    await mongoose.connect(env.DB_URL);
+    console.log("Populating...");
+
+    // @ts-ignore
+    const affiliates = await affiliatesDb.findMany({ select: ["userId"] });
+    for (const affiliate of affiliates) {
+        const { userId } = affiliate;
+        const children = await affiliatesRepo.getChildren({ userId, getManyQueries: { select: "[\"userId\"]" } });
+        const childrenCount = children.length;
+        const updatedAffiliate = await affiliatesDb.updateOne({ id: userId }, { childrenCount });
+        console.log(`${updatedAffiliate.userId} : ${updatedAffiliate.childrenCount}`);
+    }
+    console.log("Done!");
+    process.exit();
+}
+
+populate();
